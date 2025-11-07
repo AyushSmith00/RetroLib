83345bd7bed79c7e346f2de2a35a9a39c3731482
AyushSmith00 <140578065+AyushSmith00@users.noreply.github.com>
2025-11-07 14:20:37 +0530

    complete backgrounds

--- retroLib/src/components/Button/Choosebackground.tsx
+++ retroLib/src/components/Button/Choosebackground.tsx
@@ -3,7 +3,7 @@ import "./Background.css";
 
 export const Choosebackground: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const backgrounds = [
-    "bg-1",  // Stars background
+    "bg-1",
     "bg-2",
     "bg-3",
     "bg-4"
@@ -26,6 +26,31 @@ export const Choosebackground: React.FC<{ children: React.ReactNode }> = ({ chil
           <div className="shooting-star"></div>
           <div className="shooting-star"></div>
           <div className="shooting-star"></div>
+          </>
+      )}
+
+      {index === 1 && (
+        <>
+        <div className="lines">
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+          </div>
+          </>
+      )}
+
+      {index === 2 && (
+        <>
+        <div>
+          <div className="bg3"></div>
+        </div>
         </>
       )}
 
