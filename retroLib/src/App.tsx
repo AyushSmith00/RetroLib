7b5aed9a7c59f052ff5170b2d1c7a23e90d8e42b
AyushSmith00 <140578065+AyushSmith00@users.noreply.github.com>
2025-11-12 20:36:42 +0530

    adding the herosection

--- retroLib/src/App.tsx
+++ retroLib/src/App.tsx
@@ -2,16 +2,18 @@ import { TheRetro } from "./main/TheRetro";
 import { RetroButton } from "./components/RetroButton/RetroButton";
 import { RetroNavbar } from "./components/RetroNavbar/RetroNavbar";
 import { RetroCard } from "./components/RetroCard/RetroCard";
+import { HeroSection } from "./components/Sections/Hero";
 
 function App() {
   return (
     <TheRetro>
       <RetroNavbar>
-        <a href="#">Home</a>
+        <a href="hero">Home</a>
         <a href="#">Skills</a>
         <a href="#">Project</a>
         <a href="#">Contact</a>
       </RetroNavbar>
+      <HeroSection/>
       <RetroCard>
         <img src="https://st2.depositphotos.com/2927537/7026/i/950/depositphotos_70260001-stock-photo-funny-monkey-with-a-red.jpg" alt="monkey" />
         <div className="retro-card-title">monkey</div>
@@ -21,6 +23,7 @@ function App() {
         </div>
       </RetroCard>
     </TheRetro>
+    
   );
 }
 
---
+++ retroLib/src/components/Sections/Hero.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Hero.css";
+
+export const HeroSection: React.FC = () => {
+  return (
+    <section id="hero" className="hero-section">
+      <h1 className="hero-title">Ayush</h1>
+      <p className="hero-subtitle">Enthusiast For Programming</p>
+    </section>
+  );
+};
